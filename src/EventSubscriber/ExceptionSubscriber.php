2ddda211369651a4855856b6cfab527f1b19f4c5
Abdoulaye Ndoye <layendoyesn@gmail.com>
2019-08-01 09:38:39 +0000

    initialisation

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\EventSubscriber;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+        if($exception instanceof NotFoundHttpException) {
+            $data = [
+                'status' => $exception->getStatusCode(),
+                'message' => 'Resource non trouvée !!'
+            ];
+
+            $response = new JsonResponse($data);
+            $event->setResponse($response);
+        }
+        else{
+            $data = [
+                'Erreur' => $exception->getMessage()
+            ];
+            $response = new JsonResponse($data);
+            $event->setResponse($response);
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'kernel.exception' => 'onKernelException',
+        ];
+    }
+}
---
+++ var/cache/dev/twig/fa/faffa31a805109acb364e63c693be64453a116bfe8cbba4bc652207ad7af532c.php
@@ -0,0 +1,62 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* @Framework/Form/range_widget.html.php */
+class __TwigTemplate_e41a37c9d348bbcf3dfa06f658df42b4e5d52509fbc4f7296c2bbdf721467e34 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "@Framework/Form/range_widget.html.php"));
+
+        // line 1
+        echo "<?php echo \$view['form']->block(\$form, 'form_widget_simple', ['type' => isset(\$type) ? \$type : 'range']);
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@Framework/Form/range_widget.html.php";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  40 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<?php echo \$view['form']->block(\$form, 'form_widget_simple', ['type' => isset(\$type) ? \$type : 'range']);
+", "@Framework/Form/range_widget.html.php", "/home/abdoulaye/Bureau/Mes Projets Symfony/Sa transfert/symfony-API/vendor/symfony/framework-bundle/Resources/views/Form/range_widget.html.php");
+    }
+}
