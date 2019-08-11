bbf3367473b243695e11b782ea35146d8bb8e45b
Abdoulaye Ndoye <layendoyesn@gmail.com>
2019-08-11 02:12:48 +0000

    fin samedi

--- src/Controller/TransationController.php
+++ src/Controller/TransationController.php
@@ -8,10 +8,12 @@ use App\Form\EnvoieType;
 use App\Entity\Entreprise;
 use App\Entity\Transaction;
 use App\Entity\Utilisateur;
+use App\Form\TransactionType;
 use App\Entity\UserCompteActuel;
 use App\Repository\CompteRepository;
 use App\Repository\TarifsRepository;
 use App\Repository\EntrepriseRepository;
+use App\Repository\TransactionRepository;
 use Symfony\Component\HttpFoundation\Request;
 use App\Repository\UserCompteActuelRepository;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -19,9 +21,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-use App\Repository\TransactionRepository;
-use App\Form\TransactionType;
 
 class TransationController extends AbstractFOSRestController
 {
@@ -116,21 +117,29 @@ class TransationController extends AbstractFOSRestController
         if(!$retrait){
             throw new HttpException(404,'Ce code n\'existe pas !');
         }
+        elseif($retrait->getStatus()!='Envoyer'){
+            throw new HttpException(403,'Montant déja retiré !');
+        }
+
         $form = $this->createForm(TransactionType::class,$retrait);
         $form->submit($data);
+
         if(!$form->isSubmitted() || !$form->isValid()){
             return $this->handleView($this->view($validator->validate($form)));
         }
         $montant = $retrait->getMontant();
         $commissionRecep=$retrait->getCommissionEmetteur()/2;//car l'emetteur avait 20% et le recepteur doit en avoir 10 
+        
         $userComp=$repoUserComp->findUserComptActu($userConnecte);
         $retrait->setDateReception(new \DateTime())
                 ->setCommissionRecepteur($commissionRecep)
                 ->setUserComptePartenaireRecepteur($userComp)
                 ->setStatus('Retirer');
         $manager->persist($retrait);
+        
         $userComp->getCompte()->setSolde($userComp->getCompte()->getSolde()+$commissionRecep+$montant);
         $manager->persist($userComp);
+        
         $manager->flush();
         $afficher = [
            $this->message => 201,
